4d14542255e5ea5a20fa7a38ce7d4f33d414b3aa
Naufal Arif <naufalarif92@gmail.com>
2022-10-17 16:44:00 +0700

    Add style name

--- src/js/zoom.js
+++ src/js/zoom.js
@@ -11,8 +11,11 @@ let data = {
     ],
 };
 
-
 for (var i = 0; i < data.mode.length; i++) {
     let x = $('.card:first-child').clone().removeClass('zoom-basic').addClass(data.mode[i]);
     $(x).appendTo('.card-columns');
+    $('p:first-child').remove();
+    const name = document.createElement("p");
+    name.innerHTML = data.mode[i];
+    $(x).append(name);
 }
